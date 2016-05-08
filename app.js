5dbec0ceec348ca1c65fe7f468abc6a313c41da0
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-05-09 05:20:53 +1000

    Made some changes

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 var express = require('express'),
+    app = express(),
     path = require('path'),
-    app = express();
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    config = require('./config/config.js'),
+    ConnectMongo = require('connect-mongo')(session),
+    mongoose = require('mongoose').connect(config.dbURL);
 
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html',require('hogan-express'));
+app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+var env = process.env.NODE_ENV || "development";
+if (env === "development") {
+    app.use(session({
+        secret: config.sessionSecret
+    }));
+}
+else {
+    app.use(session({
+        secret: config.sessionSecret,
+        store: new ConnectMongo({
+            url: config.dbURL,
+            stringify: true
+        })
+    }));
+}
 
-require('./routes/routes.js')(express, app);
+var userSchema = mongoose.Schema({
+    username: String,
+    password: String,
+    fullname: String
+});
+var Person = mongoose.model('users', userSchema);
+var John = new Person({
+    username: "mnahm5",
+    password: "password",
+    fullname: "Md Nadim Ahmed"
+});
+John.save(function (err) {
+    console.log('Done');
+});
+
+require('./routes/routes.js')(express,app);
 
 app.listen(3000, function () {
-    console.log('Cards Against Humanity App Working on Port 3000')
+    console.log("App running on Port 3000");
+    console.log("Mode: " + env);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,23 @@
 module.exports = function (express, app) {
     var router = express.Router();
-
+    
     router.get('/', function (req, res, next) {
-        res.render('index',{
-            title: 'Welcome'
+        res.render('home', {
+            title: "Welcome to my Game"
         });
     });
+    router.get('/user', function (req, res, next) {
+        res.render('user', {
+            title: "Welcome User"
+        });
+    });
+    router.get('/setcolor', function (req, res, next) {
+        req.session.favColor = "Red";
+        res.send("Setting favourite color !");
+    });
+    router.get('/getcolor', function (req, res, next) {
+        res.send("Favourite Color: " + (req.session.favColor===undefined?"Not Found":req.session.favColor));
+    });
 
-    app.use('/', router);
+    app.use('/',router);
 };
